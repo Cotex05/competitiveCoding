5b37bc3f1dd0f2eadbcec185c85d343daa5dc2d5
Cotex05 <61322721+Cotex05@users.noreply.github.com>
2021-08-22 20:36:34 +0530

    template added.
    Simple C++ boiler plate code

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define pb push_back
+#define endl "\n"
+#define dd double
+#define mp make_pair
+#define fi first
+#define se second
+#define tc ll t;cin>>t;while(t--)
+#define ull unsigned int long long
+const int INF = INT_MAX;
+#define INFL LLONG_MAX
+#define f_scan  ios_base::sync_with_stdio(!cin.tie(0))
+#define all(x) x.begin(),x.end()
+#define mod 1000000007
+#define deci(n) fixed << setprecision(n)
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  //write code here
+  
+  
+	return 0;
+}
